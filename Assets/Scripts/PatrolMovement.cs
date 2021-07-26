af9f9ece93d5ac139d828d0eeebdc6bd61c73b39
Kirill <cyrillx@users.noreply.github.com>
2021-07-26 21:23:38 +0300

    Opossums patrol path

---
+++ Assets/Scripts/PatrolMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _path;
+    [SerializeField] private float _speed;
+
+    private Transform[] _points;
+    private int _currentPoint;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _points = new Transform[_path.childCount];
+        for (int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        Transform target = _points[_currentPoint];
+        transform.position = Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+
+        if(transform.position == target.position)
+        {
+            Debug.Log("URA");
+
+            _currentPoint++;
+            if(_currentPoint >= _points.Length)
+            {
+                _currentPoint = 0;
+            }
+
+            if (_points[_currentPoint].position.x > transform.position.x)
+                _spriteRenderer.flipX = true;
+            else
+                _spriteRenderer.flipX = false;
+        }
+    }
+}
